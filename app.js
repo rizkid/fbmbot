d919b2ed7dc04883cddb783182bc4c7be67e4ab3
rizkidermawan <rizki.dermawan@happyfresh.com>
2016-09-27 16:42:40 +0700

    use http instead of https

--- app.js
+++ app.js
@@ -222,7 +222,7 @@ actions[0] = "searchRecipe";
 
 var Action = {
   searchRecipe: function(event) {
-    var https = require('https');
+    var http = require('http');
     var param = event.message.text.substring(7);
 
     //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
@@ -245,7 +245,7 @@ var Action = {
       });
     }
 
-    https.request(options, callback).end();
+    http.request(options, callback).end();
     sendTextMessage(event.sender.id, param)
   }
 }
